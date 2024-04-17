42817705d1528fd1f562912acb13d62a49bda000
wenubey <mertfatihsimsek06@gmail.com>
2024-04-17 14:51:59 +0200

    Needed libraries implemented. CharacterRepository and UserPreferencesRepository created.

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,6 @@ plugins {
     alias(libs.plugins.jetbrainsKotlinAndroid) apply false
     alias(libs.plugins.androidLibrary) apply false
     kotlin("plugin.serialization") version libs.versions.kotlin apply false
+    alias(libs.plugins.hilt) apply false
+    alias(libs.plugins.ksp) apply false
 }
