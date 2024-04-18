84a323babcdb18ffe05fb51652df1073e6c045fb
wenubey <mertfatihsimsek06@gmail.com>
2024-04-18 21:03:39 +0200

    navigation created, dummy screens created.

--- app/src/main/java/com/wenubey/rickandmortywiki/ui/MainActivity.kt
+++ app/src/main/java/com/wenubey/rickandmortywiki/ui/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.wenubey.rickandmortywiki
+package com.wenubey.rickandmortywiki.ui
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -6,43 +6,31 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.wenubey.rickandmortywiki.ui.nav.RootNavigationGraph
 import com.wenubey.rickandmortywiki.ui.theme.RickAndMortyWikiTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private lateinit var navController: NavHostController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            navController = rememberNavController()
             RickAndMortyWikiTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    RootNavigationGraph(navController = navController)
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    RickAndMortyWikiTheme {
-        Greeting("Android")
-    }
-}
