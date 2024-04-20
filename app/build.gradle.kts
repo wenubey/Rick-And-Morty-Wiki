6ac117a0a3b5936eefd6cea80a888dc23ea93905
wenubey <mertfatihsimsek06@gmail.com>
2024-04-20 23:37:55 +0200

    somehow when project created missed the appcompat dependency added. Night Mode implementation completed on activity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,6 +55,7 @@ dependencies {
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.appcompat)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
