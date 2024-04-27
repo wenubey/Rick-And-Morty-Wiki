d1540f3326565920458f28023d2c1f7086e3440f
wenubey <mertfatihsimsek06@gmail.com>
2024-04-27 02:00:56 +0200

    Getting character's detail data functionality completed.

--- app/src/main/java/com/wenubey/rickandmortywiki/ui/viewmodels/CharacterDetailViewModel.kt
+++ app/src/main/java/com/wenubey/rickandmortywiki/ui/viewmodels/CharacterDetailViewModel.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.wenubey.domain.model.Character
+import com.wenubey.domain.model.DataPoint
 import com.wenubey.domain.repository.CharacterRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -14,24 +15,33 @@ import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+/* TODO connect to the UI
+ * TODO create UI components for detail
+ * TODO don't forget the screen compatibility
+ * TODO don't forget the handle config changes like screen orientation change
+ */
 @HiltViewModel
 class CharacterDetailViewModel @Inject constructor(
     private val characterRepository: CharacterRepository
-): ViewModel(){
+) : ViewModel() {
 
     private val _characterDetailUiState = MutableStateFlow<CharacterDetailUiState>(
         value = CharacterDetailUiState.Loading
     )
-    val characterDetailUiState: StateFlow<CharacterDetailUiState> = _characterDetailUiState.asStateFlow()
+    val characterDetailUiState: StateFlow<CharacterDetailUiState> =
+        _characterDetailUiState.asStateFlow()
+
 
     fun getCharacter(id: Int) = viewModelScope.launch {
         _characterDetailUiState.update { return@update CharacterDetailUiState.Loading }
         characterRepository.getCharacter(id)
             .onSuccess { character ->
                 Log.w(TAG, "getCharacter:Success: ${character.name}")
+                val dataPoints = createDataPoints(character)
                 _characterDetailUiState.update {
                     return@update CharacterDetailUiState.Success(
-                        character = character
+                        character = character,
+                        dataPoints = dataPoints
                     )
                 }
             }
@@ -45,6 +55,19 @@ class CharacterDetailViewModel @Inject constructor(
             }
     }
 
+    private fun createDataPoints(character: Character): List<DataPoint> {
+        return buildList {
+            add(DataPoint(title = "Last known location", description = character.location.name))
+            add(DataPoint(title = "Species", description = character.species))
+            add(DataPoint(title = "Gender", description = character.gender.displayName))
+            add(DataPoint(title = "Origin", description = character.origin.name))
+            add(DataPoint(title = "Episode count", description = character.episodeIds.size.toString()))
+            character.type.takeIf { it.isNotEmpty() }?.let { type ->
+                add(DataPoint(title = "Type", description = type))
+            }
+        }
+    }
+
     private companion object {
         const val TAG = "characterDetailViewModel"
         const val UNKNOWN_ERROR = "Unknown error occurred."
@@ -54,5 +77,8 @@ class CharacterDetailViewModel @Inject constructor(
 sealed interface CharacterDetailUiState {
     data object Loading : CharacterDetailUiState
     data class Error(val message: String) : CharacterDetailUiState
-    data class Success(val character: Character = Character.default()) : CharacterDetailUiState
+    data class Success(
+        val character: Character = Character.default(),
+        val dataPoints: List<DataPoint> = listOf()
+    ) : CharacterDetailUiState
 }
---
+++ domain/src/main/java/com/wenubey/domain/model/DataPoint.kt
@@ -0,0 +1,9 @@
+package com.wenubey.domain.model
+
+import androidx.annotation.StringRes
+import androidx.compose.ui.graphics.vector.ImageVector
+
+data class DataPoint(
+    val title: String,
+    val description: String,
+)
