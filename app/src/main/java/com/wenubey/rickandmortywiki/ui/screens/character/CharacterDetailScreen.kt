0005d71b6e03829c3616333cf9766c1af0ceca4e
wenubey <mertfatihsimsek06@gmail.com>
2024-07-05 14:58:55 +0200

    import issues

--- app/src/main/java/com/wenubey/rickandmortywiki/ui/screens/character/CharacterDetailScreen.kt
+++ app/src/main/java/com/wenubey/rickandmortywiki/ui/screens/character/CharacterDetailScreen.kt
@@ -1,26 +1,33 @@
 package com.wenubey.rickandmortywiki.ui.screens.character
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.wenubey.domain.model.Character
 import com.wenubey.rickandmortywiki.R
 import com.wenubey.rickandmortywiki.ui.components.character.detail.CharacterPropertiesComponent
 import com.wenubey.rickandmortywiki.ui.components.character.detail.CharacterStatusDetailComponent
 import com.wenubey.rickandmortywiki.ui.components.common.CommonTopAppBar
 import com.wenubey.rickandmortywiki.ui.components.common.CustomProgressIndicator
 import com.wenubey.rickandmortywiki.ui.components.pref_menu.UserPreferencesOption
+import com.wenubey.rickandmortywiki.ui.isSystemInPortraitOrientation
 import com.wenubey.rickandmortywiki.ui.makeToast
 import com.wenubey.rickandmortywiki.ui.viewmodels.CharacterDetailUiState
 import com.wenubey.rickandmortywiki.ui.viewmodels.character.CharacterDetailViewModel
@@ -86,31 +93,20 @@ fun CharacterDetailScreen(
                     )
                 }
             ) { paddingValues ->
-                LazyColumn(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(paddingValues),
-                    contentPadding = PaddingValues(16.dp),
-                    state = lazyListState,
-                ) {
-
-                    item { Spacer(modifier = Modifier.height(8.dp)) }
-
-                    item {
-                        CharacterStatusDetailComponent(
-                            characterStatus = character.status,
-                            imageUrl = character.imageUrl
-                        )
-                    }
-
-                    item {
-                        CharacterPropertiesComponent(
-                            character = character,
-                            onLocationClicked = onLocationClicked,
-                        )
-                    }
-
-
+                if (isSystemInPortraitOrientation()) {
+                    CharacterDetailPortraitScreen(
+                        paddingValues = paddingValues,
+                        lazyListState = lazyListState,
+                        character = character,
+                        onLocationClicked = onLocationClicked
+                    )
+                } else {
+                    CharacterDetailLandscapeScreen(
+                        paddingValues = paddingValues,
+                        lazyListState = lazyListState,
+                        character = character,
+                        onLocationClicked = onLocationClicked
+                    )
                 }
 
 
@@ -119,6 +115,80 @@ fun CharacterDetailScreen(
     }
 
 
+}
+
+@Composable
+fun CharacterDetailPortraitScreen(
+    paddingValues: PaddingValues,
+    lazyListState: LazyListState,
+    character: Character,
+    onLocationClicked: (String) -> Unit,
+) {
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(paddingValues),
+        contentPadding = PaddingValues(16.dp),
+        state = lazyListState,
+    ) {
+
+        item { Spacer(modifier = Modifier.height(8.dp)) }
+
+        item {
+            Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
+                CharacterStatusDetailComponent(
+                    characterStatus = character.status,
+                    imageUrl = character.imageUrl
+                )
+            }
+        }
+
+        item {
+            CharacterPropertiesComponent(
+                character = character,
+                onLocationClicked = onLocationClicked,
+            )
+        }
+
+
+    }
+}
+
+@Composable
+fun CharacterDetailLandscapeScreen(
+    paddingValues: PaddingValues,
+    lazyListState: LazyListState,
+    character: Character,
+    onLocationClicked: (String) -> Unit,
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(paddingValues),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        CharacterStatusDetailComponent(
+            characterStatus = character.status,
+            imageUrl = character.imageUrl
+        )
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize(),
+            contentPadding = PaddingValues(16.dp),
+            state = lazyListState
+        ) {
+            item {
+                CharacterPropertiesComponent(
+                    character = character,
+                    onLocationClicked = onLocationClicked,
+                )
+            }
+
+
+        }
+
+
+    }
 }
 
 
