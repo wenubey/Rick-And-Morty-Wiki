59845f2a499b96f95a50526d13074e2979a0ab47
wenubey <mertfatihsimsek06@gmail.com>
2024-07-20 16:54:22 +0200

    Feature: Dispatchers injected into viewmodels.

--- app/src/main/java/com/wenubey/rickandmortywiki/ui/di/CoroutineModule.kt
+++ app/src/main/java/com/wenubey/rickandmortywiki/ui/di/CoroutineModule.kt
@@ -3,7 +3,6 @@ package com.wenubey.rickandmortywiki.ui.di
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.scopes.ViewModelScoped
 import dagger.hilt.components.SingletonComponent
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
