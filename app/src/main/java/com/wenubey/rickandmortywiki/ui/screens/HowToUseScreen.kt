313c65b39d9771a57aaaaa119c4b22ac10d7917b
wenubey <mertfatihsimsek06@gmail.com>
2024-07-22 02:45:57 +0200

    BUG-FIX: Forgot to implement CommonTopAppBar into HowToUseScreen screen.

--- app/src/main/java/com/wenubey/rickandmortywiki/ui/screens/HowToUseScreen.kt
+++ app/src/main/java/com/wenubey/rickandmortywiki/ui/screens/HowToUseScreen.kt
@@ -1,30 +1,16 @@
 package com.wenubey.rickandmortywiki.ui.screens
 
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.LineBreak
 import androidx.compose.ui.unit.dp
 import com.wenubey.rickandmortywiki.R
 import com.wenubey.rickandmortywiki.ui.components.common.CommonTopAppBar
@@ -32,7 +18,6 @@ import com.wenubey.rickandmortywiki.ui.components.how_to_use.FeedbackText
 import com.wenubey.rickandmortywiki.ui.components.how_to_use.VideoSectionDivider
 import com.wenubey.rickandmortywiki.ui.components.how_to_use.VideoSectionHorizontal
 import com.wenubey.rickandmortywiki.ui.components.how_to_use.VideoSectionVertical
-import com.wenubey.rickandmortywiki.ui.viewmodels.settings.VideoPlayer
 
 
 @OptIn(ExperimentalMaterial3Api::class)
