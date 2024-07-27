f5ae3e0223c8d09d45c2f06e1e3104a80dd651af
wenubey <mertfatihsimsek06@gmail.com>
2024-07-27 21:15:27 +0200

    Version code changed.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         applicationId = "com.wenubey.rickandmortywiki"
         minSdk = 26
         targetSdk = 34
-        versionCode = 7
+        versionCode = 8
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
